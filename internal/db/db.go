933eef9d0db5923002a3ba8e6ca865181aa7fbf1
Nathan Higley <contact@nathanhigley.com>
2025-03-22 19:09:37 -0500

    feat: add reading and writing db to file

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,7 +1,10 @@
 package db
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/astr0n8t/gutenberg-ingester/internal/history"
+	"os"
 )
 
 func NewDB() *DB {
@@ -9,4 +12,95 @@ func NewDB() *DB {
 		Version:  1,
 		Download: *history.NewHistory(),
 	}
+}
+
+func OpenDBFromFile(filename string) (*DB, error) {
+
+	var db DB
+
+	fileInfo, fileInfoErr := os.Stat(filename)
+	if os.IsNotExist(fileInfoErr) {
+		file, fileCreateErr := os.Create(filename)
+		if fileCreateErr != nil {
+			return nil, fmt.Errorf("failed to create download database file: %v with error: %v", filename, fileCreateErr)
+		}
+		// If we can create the file, hopefully we can read the file
+		fileInfo, fileInfoErr = os.Stat(filename)
+		if fileInfoErr != nil {
+			return nil, fmt.Errorf("failed to read created download database file: %v with error: %v", filename, fileInfoErr)
+		}
+		file.Close()
+
+		db = *NewDB()
+		return &db, db.WriteDBToFile(filename)
+	}
+
+	file, fileOpenErr := os.Open(filename)
+	if fileOpenErr != nil {
+		return nil, fmt.Errorf("failed to open download database file: %v with error: %v", filename, fileOpenErr)
+	}
+
+	jsonData := make([]byte, fileInfo.Size())
+	_, fileReadErr := file.Read(jsonData)
+	if fileReadErr != nil {
+		return nil, fmt.Errorf("failed to read download database file: %v with error: %v", filename, fileReadErr)
+	}
+
+	jsonErr := json.Unmarshal(jsonData, &db)
+	if jsonErr != nil {
+		return nil, fmt.Errorf("failed to unmarshal download database file: %v with error: %v", filename, fileReadErr)
+	}
+
+	if db.Version != 1 {
+		return nil, fmt.Errorf("unknown database file version: %v when opening file: %v", db.Version, filename)
+	}
+
+	return &db, nil
+}
+
+func (d *DB) WriteDBToFile(filename string) error {
+	fileInfo, fileInfoErr := os.Stat(filename)
+	if os.IsNotExist(fileInfoErr) {
+		file, fileCreateErr := os.Create(filename)
+		if fileCreateErr != nil {
+			return fmt.Errorf("failed to create download database file: %v with error: %v", filename, fileCreateErr)
+		}
+		// If we can create the file, hopefully we can read the file
+		fileInfo, fileInfoErr = os.Stat(filename)
+		if fileInfoErr != nil {
+			return fmt.Errorf("failed to read created download database file: %v with error: %v", filename, fileInfoErr)
+		}
+		file.Close()
+	}
+
+	file, fileErr := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_TRUNC, fileInfo.Mode())
+	if fileErr != nil {
+		fmt.Errorf("failed to open download database file: %v with error: %v", filename, fileErr)
+	}
+
+	jsonData, jsonErr := json.MarshalIndent(d, "", " ")
+	if jsonErr != nil {
+		fmt.Errorf("unable to marshal DB to json %v", jsonErr)
+	}
+
+	_, writeErr := file.Write(jsonData)
+	if writeErr != nil {
+		fmt.Errorf("unable to write DB to download database file: %v with error: %v", filename, writeErr)
+	}
+
+	file.Close()
+
+	return nil
+}
+
+func (d *DB) SetDownloaded(id int) {
+	d.Download.SetHistory(id)
+}
+
+func (d *DB) UnsetDownloaded(id int) {
+	d.Download.UnsetHistory(id)
+}
+
+func (d *DB) GetDownloaded(id int) bool {
+	return d.Download.GetHistory(id)
 }
--- internal/db/types_test.go
+++ internal/db/types_test.go
@@ -3,6 +3,7 @@ package db
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"testing"
 )
 
@@ -24,4 +25,41 @@ func TestUnMarshalDB(t *testing.T) {
 	if db.Version != 1 {
 		t.Errorf("Unable to unmarshal empty DB from json")
 	}
+}
+
+func TestWriteDB(t *testing.T) {
+	db := NewDB()
+
+	testFile := "/tmp/gutenberg_test1_db.json"
+	err := db.WriteDBToFile(testFile)
+
+	if err != nil {
+		t.Errorf("Unable to write empty DB to json file %v", err)
+	}
+
+	os.Remove(testFile)
+}
+
+func TestOpenDB(t *testing.T) {
+	testFile := "/tmp/gutenberg_test2_db.json"
+	db1, err1 := OpenDBFromFile("/tmp/gutenberg_test_db.json")
+
+	if err1 != nil {
+		t.Errorf("unable to write and read empty DB %v", err1)
+	}
+
+	if db1.GetDownloaded(1) {
+		t.Errorf("created and read in DB file is not empty")
+	}
+
+	db2, err2 := OpenDBFromFile("/tmp/gutenberg_test_db.json")
+
+	if err2 != nil {
+		t.Errorf("unable to read empty DB %v", err2)
+	}
+
+	if db2.GetDownloaded(1) {
+		t.Errorf("read in DB file is not empty")
+	}
+	os.Remove(testFile)
 }
